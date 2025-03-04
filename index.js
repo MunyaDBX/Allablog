ecb7dea8ffa4c651ddd23b2f2d88d095875c1887
unknown <akambati2011@gmail.com>
2025-03-05 00:12:59 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+var myBlog = " "
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/home", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  myBlog = req.body["postText"]
+  res.render("index.ejs", {lBlog: myBlog});
+});
+
+app.get("/save", (req, res) => {
+  myBlog = req.body['postEdit'];
+  res.render("browse.ejs", {lBlog: myBlog});
+});
+
+app.get("/browse", (req, res) => {
+  res.render("browse.ejs", {lBlog: myBlog});
+});
+
+app.get("/delete", (req, res) => {
+  myBlog = " ";
+  res.render("browse.ejs", {lBlog: myBlog});
+});
+
+app.get("/cancel", (req, res) => {
+  res.render("browse.ejs", {lBlog: myBlog});
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
